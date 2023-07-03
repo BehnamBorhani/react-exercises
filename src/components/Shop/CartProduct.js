725d74410ef22cae99496782fbfc2d4893332710
Behnam <behnamborhani95@gmail.com>
2023-07-03 16:28:29 +0330

    base code of shopping cart project created

---
+++ src/components/Shop/CartProduct.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class CartProduct extends Component {
+   render() {
+      return (
+         <div className="cart-row">
+            <div className="cart-item cart-column">
+               <img
+                  className="cart-item-image"
+                  src="Images/Shirt.png"
+                  width="100"
+                  height="100"
+               />
+               <span className="cart-item-title">Shirt</span>
+            </div>
+            <span className="cart-price cart-column">$198</span>
+            <div className="cart-quantity cart-column">
+               <button className="btn btn-danger" type="button">
+                  REMOVE
+               </button>
+            </div>
+         </div>
+      );
+   }
+}
---
+++ src/components/Shop/Product.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Product extends Component {
+   render() {
+      return (
+         <div className="shop-item">
+            <span className="shop-item-title">Shirt</span>
+            <img className="shop-item-image" src="Images/Shirt.png" />
+            <div className="shop-item-details">
+               <span className="shop-item-price">$129</span>
+               <button className="btn btn-primary shop-item-button" type="button">
+                  ADD TO CART
+               </button>
+            </div>
+         </div>
+      );
+   }
+}
