67a13d3595b6ee4f84f29504eb7a462667274586
Behnam <behnamborhani95@gmail.com>
2023-07-12 09:10:34 +0330

    base code of note app project created

---
+++ src/components/NoteApp/ColorBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class ColorBox extends Component {
+   render() {
+      return (
+         <div className="color-box" style={{ backgroundColor: "red" }}></div>
+      );
+   }
+}
