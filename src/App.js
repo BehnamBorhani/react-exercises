8c05367832d03b8cf16c606d645dc07ea4003a12
Behnam <behnamborhani95@gmail.com>
2023-07-14 18:42:46 +0330

    refactor with react functional components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
-import Form from './components/Form/Form'
+import React from "react";
+import Form from "./components/Form/Form";
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Form />
-            </div>
-        )
-    }
+export default function App() {
+   return (
+      <div>
+         <Form />
+      </div>
+   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,104 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-export default class App extends React.Component {
-   constructor(props) {
-      super(props);
+export default function Form() {
+   const [firstNameData, setFirstNameData] = useState("");
+   const [lastNameData, setLastNameData] = useState("");
+   const [emailData, setEmailData] = useState("");
+   const [submitted, setSubmitted] = useState(false);
+   const [allValid, setAllValid] = useState(false);
 
-      this.state = {
-         firstNameData: "",
-         lastNameData: "",
-         emailData: "",
-         submitted: false,
-         allValid: false,
-      };
-   }
-
-   submitHandler = (event) => {
+   const submitHandler = (event) => {
       event.preventDefault();
-      this.setState({ submitted: true });
+      setSubmitted(true);
 
-      const { firstNameData, lastNameData, emailData } = this.state;
       if (firstNameData && lastNameData && emailData) {
-         this.setState({ allValid: true });
-
+         setAllValid(true);
          setTimeout(() => {
-            this.setState({
-               allValid: false,
-            });
+            setAllValid(false);
          }, 3000);
       }
    };
 
-   firstNameHandler = (event) => {
-      this.setState({ firstNameData: event.target.value });
-   };
-
-   lastNameHandler = (event) => {
-      this.setState({ lastNameData: event.target.value });
-   };
-
-   emailNameHandler = (event) => {
-      this.setState({ emailData: event.target.value });
-   };
-
-   render() {
-      const { firstNameData, lastNameData, emailData, submitted, allValid } =
-         this.state;
-      return (
-         <div className="form-container">
-            <form
-               className="register-form"
-               autoComplete="off"
-               onSubmit={this.submitHandler}
-            >
-               {/* Uncomment the next line to show the success message */}
-               {submitted && allValid && (
-                  <div className="success-message">
-                     Success! Thank you for registering
-                  </div>
-               )}
-               <input
-                  id="first-name"
-                  className="form-field"
-                  type="text"
-                  placeholder="First Name"
-                  name="firstName"
-                  onChange={this.firstNameHandler}
-               />
-               {/* Uncomment the next line to show the error message */}
-               {submitted && !firstNameData && (
-                  <span id="first-name-error">Please enter a first name</span>
-               )}
-               <input
-                  id="last-name"
-                  className="form-field"
-                  type="text"
-                  placeholder="Last Name"
-                  name="lastName"
-                  onChange={this.lastNameHandler}
-               />
-               {/* Uncomment the next line to show the error message */}
-               {submitted && !lastNameData && (
-                  <span id="last-name-error">Please enter a last name</span>
-               )}
-               <input
-                  id="email"
-                  className="form-field"
-                  type="text"
-                  placeholder="Email"
-                  name="email"
-                  onChange={this.emailNameHandler}
-               />
-               {/* Uncomment the next line to show the error message */}
-               {submitted && !emailData && (
-                  <span id="email-error">Please enter an email address</span>
-               )}
-               <button className="form-field" type="submit">
-                  Register
-               </button>
-            </form>
-         </div>
-      );
-   }
+   return (
+      <div className="form-container">
+         <form
+            className="register-form"
+            autoComplete="off"
+            onSubmit={submitHandler}
+         >
+            {/* Uncomment the next line to show the success message */}
+            {submitted && allValid && (
+               <div className="success-message">
+                  Success! Thank you for registering
+               </div>
+            )}
+            <input
+               id="first-name"
+               className="form-field"
+               type="text"
+               placeholder="First Name"
+               name="firstName"
+               onChange={(event) => setFirstNameData(event.target.value)}
+            />
+            {/* Uncomment the next line to show the error message */}
+            {submitted && !firstNameData && (
+               <span id="first-name-error">Please enter a first name</span>
+            )}
+            <input
+               id="last-name"
+               className="form-field"
+               type="text"
+               placeholder="Last Name"
+               name="lastName"
+               onChange={(event) => setLastNameData(event.target.value)}
+            />
+            {/* Uncomment the next line to show the error message */}
+            {submitted && !lastNameData && (
+               <span id="last-name-error">Please enter a last name</span>
+            )}
+            <input
+               id="email"
+               className="form-field"
+               type="text"
+               placeholder="Email"
+               name="email"
+               onChange={(event) => setEmailData(event.target.value)}
+            />
+            {/* Uncomment the next line to show the error message */}
+            {submitted && !emailData && (
+               <span id="email-error">Please enter an email address</span>
+            )}
+            <button className="form-field" type="submit">
+               Register
+            </button>
+         </form>
+      </div>
+   );
 }
