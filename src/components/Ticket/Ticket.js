e7d9731e280e3fa082d4c72944f076a78ea4e359
Behnam <behnamborhani95@gmail.com>
2023-07-19 11:40:22 +0330

    refactor with functional component

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,71 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Ticket.css";
 
-export default class Ticket extends React.Component {
-   constructor(props) {
-      super(props);
+export default function Ticket() {
+   const [countriesData, setCountriesData] = useState({
+      Iran: ["Tabriz", "Tehran", "Shiraz", "Esfahan", "Mashhad"],
+      Turkey: ["Istanbul", "Ezmir", "Ankara", "Antaliya"],
+      US: ["Los Angles", "San Diego", "Chicago"],
+   });
+   const [mainCountryCities, setMainCountryCities] = useState([]);
 
-      this.state = {
-         countriesData: {
-            Iran: ["Tabriz", "Tehran", "Shiraz", "Esfahan", "Mashhad"],
-            Turkey: ["Istanbul", "Ezmir", "Ankara", "Antaliya"],
-            US: ["Los Angles", "San Diego", "Chicago"],
-         },
-         mainCountryCities: [],
-      };
-   }
-
-   countryHandler = (event) => {
+   const countryHandler = (event) => {
       const country = event.target.value;
-      this.setState({ mainCountryCities: this.state.countriesData[country] });
+      setMainCountryCities(countriesData[country]);
    };
 
-   render() {
-      return (
-         <div className="container">
-            <div className="col-md-6 box">
-               <input className="fnameInput" placeholder="First Name" />
-            </div>
-
-            <div className="col-md-6 box">
-               <input className="lnameInput" placeholder="Last Name" />
-            </div>
-            <div className="col-md-6 box">
-               <input className="phoneInput" placeholder="Phone Number" />
-            </div>
-            <div className="col-md-6 box">
-               <input className="emailInput" placeholder="Email" />
-            </div>
-            <div className="col-md-6 box">
-               <select className="countrySelect" onChange={this.countryHandler}>
-                  <option value="-1">Please Select ...</option>
-                  <option className="option" value="Iran">
-                     Iran
-                  </option>
-                  <option className="option" value="Turkey">
-                     Turkey
-                  </option>
-                  <option className="option" value="US">
-                     United State
-                  </option>
-               </select>
-            </div>
-            <div className="col-md-6 box">
-               <select className="citySelect">
-                  <option value="-1">Please Select City</option>
-                  {this.state.mainCountryCities.map((city, index) => {
-                     return (
-                        <option value={index} key={index}>
-                           {city}
-                        </option>
-                     );
-                  })}
-               </select>
-            </div>
-            <div className="col-md-12 box">
-               <button className="btn">Book a ticket</button>
-            </div>
+   return (
+      <div className="container">
+         <div className="col-md-6 box">
+            <input className="fnameInput" placeholder="First Name" />
          </div>
-      );
-   }
+
+         <div className="col-md-6 box">
+            <input className="lnameInput" placeholder="Last Name" />
+         </div>
+         <div className="col-md-6 box">
+            <input className="phoneInput" placeholder="Phone Number" />
+         </div>
+         <div className="col-md-6 box">
+            <input className="emailInput" placeholder="Email" />
+         </div>
+         <div className="col-md-6 box">
+            <select className="countrySelect" onChange={countryHandler}>
+               <option value="-1">Please Select ...</option>
+               <option className="option" value="Iran">
+                  Iran
+               </option>
+               <option className="option" value="Turkey">
+                  Turkey
+               </option>
+               <option className="option" value="US">
+                  United State
+               </option>
+            </select>
+         </div>
+         <div className="col-md-6 box">
+            <select className="citySelect">
+               <option value="-1">Please Select City</option>
+               {mainCountryCities.map((city) => {
+                  return (
+                     <option value={city} key={city}>
+                        {city}
+                     </option>
+                  );
+               })}
+            </select>
+         </div>
+         <div className="col-md-12 box">
+            <button className="btn">Book a ticket</button>
+         </div>
+      </div>
+   );
 }
